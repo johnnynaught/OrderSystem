a09d7f68c7308f7183aba75fdb21d6ac849e307f
johnnynaught <johnnyif@foxmail.com>
2025-01-09 22:19:05 -0500

    Initial commit

---
+++ src/test/java/java/projects/ordersystembackend/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package java.projects.ordersystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
